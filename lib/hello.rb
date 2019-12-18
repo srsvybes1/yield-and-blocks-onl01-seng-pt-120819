bec21cf2e8f0a1de7d8c86d0a3b80696f2926be5
Cody Toney-Griffith <cody.toney@gmail.com>
2019-12-18 23:06:16 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,11 +4,11 @@ while i < array.length
 yield (array[i])
 i = i + 1 
 end
-
+array
 
 end 
 # call your method here!
-[]
+["Tim", "Tom", "Jim"]
 hello_t(["Tim", "Tom", "Jim"]) do 
   |name|
   if name.start_with?("T")
